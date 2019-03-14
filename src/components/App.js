6a40993fd88640a55fed3a69db316dfd4c9b3267
Joe Ashworth <joeashworth@gmail.com>
2019-03-13 21:02:41 -0500

    Install axios, move state to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import axios from 'axios'
 import SearchBar from './SearchBar'
 
-const App = () => {
-  return (
-    <div
-      className="ui container"
-      style={{ marginTop: '1rem' }}>
-      <SearchBar />
-    </div>
-  )
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term)
+  }
+
+  render() {
+    return (
+      <div
+        className="ui container"
+        style={{ marginTop: '1rem' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    )
+  }
 }
 
 export default App
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,22 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+  state = { term: '' }
+
+  onFormSubmit = event => {
+    event.preventDefault()
+    this.props.onSubmit(this.state.term)
+  }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form" action="">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <label htmlFor="">Image Search</label>
-          <input className="field" type="text" />
+          <input
+            type="text"
+            value={this.state.term}
+            onChange={(e) => this.setState({ term: e.target.value })} />
         </form>
       </div>
     )
